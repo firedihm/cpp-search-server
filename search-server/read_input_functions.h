4fadba45a046bf2854bb5a6048248d4ec9e3b1d9
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-26 16:21:10 +0300

    Create read_input_functions.h

---
+++ search-server/read_input_functions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+{
+    using namespace std;
+    
+    string ReadLine();
+    int ReadLineWithNumber();
+    vector<string> SplitIntoWords(const string& text);
+}
