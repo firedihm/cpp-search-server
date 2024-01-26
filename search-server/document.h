c540d0ed0085a29a877e25600a8cf69343dbee72
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-26 17:11:08 +0300

    Create document.h

---
+++ search-server/document.h
@@ -0,0 +1,23 @@
+#pragma once
+
+{
+    using namespace std;
+    
+    enum class DocumentStatus {
+        ACTUAL,
+        IRRELEVANT,
+        BANNED,
+        REMOVED,
+    };
+    
+    struct Document {
+        int id = 0;
+        double relevance = 0.0;
+        int rating = 0;
+        
+        Document() = default;
+        Document(int document_id, double relevance, int rating) : id(document_id), relevance(relevance), rating(rating) {}
+        
+        ostream& operator<<(ostream& os) const;
+    };
+}
