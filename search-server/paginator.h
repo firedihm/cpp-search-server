e8a8b7814d3979a7f6799bfc0dda21cee3dc9dec
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-26 19:11:16 +0300

    Update paginator.h

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -1,39 +1,43 @@
 #pragma once
 
-template <typename Iterator>
-class Paginator {
-public:
-    Paginator(Iterator begin, Iterator end, size_t page_size) {
-        size_t docs_left = distance(begin, end);
-        while (docs_left) {
-            size_t docs_per_current_page = min(page_size, docs_left);
-            pages_.emplace_back(begin, begin + docs_per_current_page);
-            
-            begin += docs_per_current_page;
-            docs_left -= docs_per_current_page;
-        }
-    }
+{
+    using namespace std;
     
-    auto   begin() const { return pages_.begin(); }
-    auto   end()   const { return pages_.end(); }
-    bool   empty() const { return pages_.empty(); }
-    size_t size()  const { return pages_.size(); }
-    
-private:
-    struct IteratorRange {
-        Iterator begin;
-        Iterator end;
-        
-        IteratorRange(Iterator begin, Iterator end) : begin(begin), end(end) {}
-        
-        friend ostream& operator<<(ostream& os, const IteratorRange& range) {
-            for (auto it = range.begin; it != range.end; ++it) {
-                //помним, что it указывает на Document
-                os << *it;
+    template <typename Iterator>
+    class Paginator {
+    public:
+        Paginator(Iterator begin, Iterator end, size_t page_size) {
+            size_t docs_left = distance(begin, end);
+            while (docs_left) {
+                size_t docs_per_current_page = min(page_size, docs_left);
+                pages_.emplace_back(begin, begin + docs_per_current_page);
+                
+                begin += docs_per_current_page;
+                docs_left -= docs_per_current_page;
             }
-            return os;
         }
+        
+        auto   begin() const { return pages_.begin(); }
+        auto   end()   const { return pages_.end(); }
+        bool   empty() const { return pages_.empty(); }
+        size_t size()  const { return pages_.size(); }
+        
+    private:
+        struct IteratorRange {
+            Iterator begin;
+            Iterator end;
+            
+            IteratorRange(Iterator begin, Iterator end) : begin(begin), end(end) {}
+            
+            friend ostream& operator<<(ostream& os, const IteratorRange& range) {
+                for (auto it = range.begin; it != range.end; ++it) {
+                    //помним, что it указывает на Document
+                    os << *it;
+                }
+                return os;
+            }
+        };
+        
+        vector<IteratorRange> pages_;
     };
-    
-    vector<IteratorRange> pages_;
-};
+}
