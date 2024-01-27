a825027045df6b9ccaaff69edd06c7edfc8ecab4
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-27 22:36:05 +0300

    Update string_processing.h

--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -2,7 +2,6 @@
 
 #include "search_server.h"
 
-#include <iostream>
 #include <set>
 #include <vector>
 
@@ -12,21 +11,17 @@
     string ReadLine();
     int ReadLineWithNumber();
     
-    vector<string> SplitIntoWords(const string& text);
-    vector<string> SplitIntoWordsNoStop(const string& text, const SearchServer& search_server);
-    
     bool IsValidWord(const string& word);
-    bool IsStopWord(const string& word, const SearchServer& search_server);
+    bool IsStopWord(const string& word, const SearchServer* search_server);
+    
+    vector<string> SplitIntoWords(const string& text);
+    vector<string> SplitIntoWordsNoStop(const string& text, const SearchServer* search_server);
     
     template <typename StringContainer>
     set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
         set<string> non_empty_strings;
         for (const string& str : strings) {
-            if (!IsValidWord(str)) {
-                throw invalid_argument("stop words contain illegal characters"s);
-            }
-            
-            if (!str.empty()) {
+            if (!str.empty() && IsValidWord(str)) {
                 non_empty_strings.insert(str);
             }
         }
