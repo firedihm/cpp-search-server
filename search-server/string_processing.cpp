c1fff6777bad18ea0f9ec03518d64fdbd802d951
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:15:15 +0300

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -3,41 +3,39 @@
 #include <algorithm>
 #include <iostream>
 
-namespace std {
-    string ReadLine() {
-        string s;
-        getline(cin, s);
-        return s;
-    }
-    
-    int ReadLineWithNumber() {
-        int result;
-        cin >> result;
-        ReadLine();
-        return result;
-    }
-    
-    bool IsValidWord(const string& word) {
-        return none_of(word.begin(), word.end(), [](char c) { return c >= '\0' && c < ' '; });
-    }
-    
-    vector<string> SplitIntoWords(const string& text) {
-        vector<string> words;
-        string word;
-        for (const char c : text) {
-            if (c == ' ' || c == '\t') {
-                if (!word.empty()) {
-                    words.push_back(word);
-                    word.clear();
-                }
-            } else {
-                word += c;
+std::string ReadLine() {
+    std::string s;
+    getline(std::cin, s);
+    return s;
+}
+
+int ReadLineWithNumber() {
+    int result;
+    std::cin >> result;
+    ReadLine();
+    return result;
+}
+
+bool IsValidWord(const std::string& word) {
+    return none_of(word.begin(), word.end(), [](char c) { return c >= '\0' && c < ' '; });
+}
+
+std::vector<std::string> SplitIntoWords(const std::string& text) {
+    std::vector<std::string> words;
+    std::string word;
+    for (const char c : text) {
+        if (c == ' ' || c == '\t') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
             }
+        } else {
+            word += c;
         }
-        if (!word.empty()) {
-            words.push_back(word);
-        }
-        
-        return words;
     }
-} //namespace std
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+    
+    return words;
+}
