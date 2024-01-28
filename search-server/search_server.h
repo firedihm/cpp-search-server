39d3be2e0f88a7ae8115fee9e3b442438931ef80
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 19:18:17 +0300

    Update search_server.h

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -10,22 +10,25 @@
 #include <string>
 #include <vector>
 
+/*
+    большой бардак
+    TODO найти способ вынести реализации шаблонов
+*/
+
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
 const double EPSILON = 1e-6;
 
 class SearchServer {
 public:
     template <typename StringContainer>
-    explicit SearchServer(const StringContainer& stop_words) : stop_words_(MakeUniqueNonEmptyStrings(stop_words)) {}
+    explicit SearchServer(const StringContainer& stop_words) : stop_words_(ParseStopWords(stop_words)) {}
     explicit SearchServer(const std::string &stop_words) : SearchServer(SplitIntoWords(stop_words)) {}
     
     const std::set<std::string>& GetStopWords() const;
-    std::size_t GetDocumentCount() const;
+    size_t GetDocumentCount() const;
     int GetDocumentId(uint n) const;
     
-    bool IsStopWord(const std::string& word) const;
-    
-    bool AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings);
+    void AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings);
     
     std::tuple<std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query, int document_id) const;
     
@@ -40,8 +43,8 @@ public:
     std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentPredicate predicate) const {
         std::vector<Document> matched_documents = FindAllDocuments(ParseQuery(raw_query), predicate);
         
-        sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
-             if (abs(lhs.relevance - rhs.relevance) < EPSILON) {
+        std::sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
+             if (std::abs(lhs.relevance - rhs.relevance) < EPSILON) {
                  return lhs.rating > rhs.rating;
              }
              return lhs.relevance > rhs.relevance;
@@ -64,20 +67,37 @@ private:
     std::map<int, DocumentData> documents_;
     std::vector<int> document_ids_;
     
+    static bool IsValidWord(const std::string& word);
+    bool IsStopWord(const std::string& word) const;
+    
     static int ComputeAverageRating(const std::vector<int>& ratings);
     double ComputeWordInverseDocumentFreq(const std::string& word) const;
-    std::vector<std::string> SplitIntoWordsNoStop(const std::string& text) const;
+    
+    template <typename StringContainer>
+    std::set<std::string> ParseStopWords(const StringContainer& strings) {
+        std::set<std::string> non_empty_strings;
+        for (const std::string& str : strings) {
+            if (!str.empty()) {
+                if (!IsValidWord(str)) {
+                    using namespace std::string_literals;
+                    throw std::invalid_argument("invalid word "s + str + " was passed as stop word"s);
+                }
+                non_empty_strings.insert(str);
+            }
+        }
+        return non_empty_strings;
+    }
+    std::vector<std::string> ParseDocument(const std::string& text) const;
     
     //TODO выделить в class Query : public SearchServer
     struct QueryWord {
         std::string data;
         bool is_minus;
         bool is_stop;
-        bool is_valid;
     };
     
     QueryWord ParseQueryWord(std::string text) const {
-        bool is_minus = false, is_valid = true;
+        bool is_minus = false;
         if (text[0] == '-') {
             is_minus = true;
             text = text.substr(1);
@@ -85,10 +105,11 @@ private:
         
         //если после отрубания минуса осталась пустота или ещё один минус или слово содержит спецсимволы
         if (text.empty() || text[0] == '-' || !IsValidWord(text)) {
-            throw std::invalid_argument("query contains illegal characters");
+            using namespace std::string_literals;
+            throw std::invalid_argument("invalid word "s + text + " was passed to query"s);
         }
         
-        return {text, is_minus, IsStopWord(text), is_valid};
+        return {text, is_minus, IsStopWord(text)};
     }
     
     struct Query {
