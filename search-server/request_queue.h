a13c3da4f432e8a97f9c2c80deb55dff7c7c1f9c
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:19:12 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -2,55 +2,53 @@
 
 #include <queue>
 
-namespace std {
-    const static size_t MIN_IN_DAY = 1440;
+const static size_t MIN_IN_DAY = 1440;
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server)  : search_server_(search_server), no_result_requests_(0) {}
     
-    class RequestQueue {
-    public:
-        explicit RequestQueue(const SearchServer& search_server)  : search_server_(search_server), no_result_requests_(0) {}
+    uint GetNoResultRequests() const;
+    
+    //вызов без явно переданного статуса или предиката = вызов с предикатом по умолчанию
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus sought_status = DocumentStatus::ACTUAL) {
+        return AddFindRequest(raw_query, [=](int document_id, DocumentStatus status, int rating) {
+                                                  return status == sought_status;
+                                              });
+    }
+    
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate predicate) {
+        vector<Document> result = search_server_.FindTopDocuments(raw_query, predicate);
         
-        uint GetNoResultRequests() const;
-        
-        //вызов без явно переданного статуса или предиката = вызов с предикатом по умолчанию
-        vector<Document> AddFindRequest(const string& raw_query, DocumentStatus sought_status = DocumentStatus::ACTUAL) {
-            return AddFindRequest(raw_query, [=](int document_id, DocumentStatus status, int rating) {
-                                                      return status == sought_status;
-                                                  });
+        //избавляемся от записей, старше суток
+        if (requests_.size() == MIN_IN_DAY) {
+            if (requests_.front().empty()) {
+                --no_result_requests_;
+            }
+            requests_.pop();
         }
         
-        template <typename DocumentPredicate>
-        vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate predicate) {
-            vector<Document> result = search_server_.FindTopDocuments(raw_query, predicate);
-            
-            //избавляемся от записей, старше суток
-            if (requests_.size() == MIN_IN_DAY) {
-                if (requests_.front().empty()) {
-                    --no_result_requests_;
-                }
-                requests_.pop();
-            }
-            
-            requests_.emplace(raw_query, result.size());
-            
-            if (result.empty()) {
-                ++no_result_requests_;
-            }
-            
-            return result;
+        requests_.emplace(raw_query, result.size());
+        
+        if (result.empty()) {
+            ++no_result_requests_;
         }
         
-    private:
-        struct QueryResult {
-            string query;
-            bool is_empty;
-            
-            QueryResult(const string& query, int documents_found) : query(query), is_empty(!documents_found) {}
-            
-            bool empty() { return is_empty; }
-        };
+        return result;
+    }
+    
+private:
+    struct QueryResult {
+        std::string query;
+        bool is_empty;
         
-        const SearchServer& search_server_;
-        queue<QueryResult> requests_;
-        uint no_result_requests_;
+        QueryResult(const std::string& query, int documents_found) : query(query), is_empty(!documents_found) {}
+        
+        bool empty() { return is_empty; }
     };
-} //namespace std
+    
+    const SearchServer& search_server_;
+    std::queue<QueryResult> requests_;
+    uint no_result_requests_;
+};
