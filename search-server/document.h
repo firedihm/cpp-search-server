513bfbfd0822ed4225c3cbd25155f607da7361aa
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:09:25 +0300

    Update document.h

--- search-server/document.h
+++ search-server/document.h
@@ -2,22 +2,20 @@
 
 #include <iostream>
 
-namespace std {
-    enum class DocumentStatus {
-        ACTUAL,
-        IRRELEVANT,
-        BANNED,
-        REMOVED,
-    };
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+struct Document {
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
     
-    struct Document {
-        int id = 0;
-        double relevance = 0.0;
-        int rating = 0;
-        
-        Document() = default;
-        Document(int document_id, double relevance, int rating) : id(document_id), relevance(relevance), rating(rating) {}
-        
-        ostream& operator<<(ostream& os) const;
-    };
-} //namespace std
+    Document() = default;
+    Document(int document_id, double relevance, int rating) : id(document_id), relevance(relevance), rating(rating) {}
+    
+    std::ostream& operator<<(std::ostream& os) const;
+};
