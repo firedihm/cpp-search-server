82e33aa610dfeb9a973ef519903221836b9aa337
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:09:39 +0300

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -1,13 +1,11 @@
 #include "document.h"
 
-namespace std {
-    ostream& Document::operator<<(ostream& os) const {
-        os << "{ "s
-           << "document_id = "s << this->id << ", "s
-           << "relevance = "s << this->relevance << ", "s
-           << "rating = "s << this->rating
-           << " }"s;
-        
-        return os;
-    }
-} //namespace std
+std::ostream& Document::operator<<(std::ostream& os) const {
+    os << "{ "s
+        << "document_id = "s << this->id << ", "s
+        << "relevance = "s << this->relevance << ", "s
+        << "rating = "s << this->rating
+        << " }"s;
+     
+    return os;
+ }
