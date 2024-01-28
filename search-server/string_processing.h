e7ef65e9e68f8c1fd2797f5a6b90f018f7482df0
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 18:39:39 +0300

    Update string_processing.h

--- search-server/string_processing.h
+++ search-server/string_processing.h
@@ -1,23 +1,9 @@
 #pragma once
 
-#include <set>
 #include <string>
 #include <vector>
 
 std::string ReadLine();
 int ReadLineWithNumber();
 
-bool IsValidWord(const std::string& word);
-
 std::vector<std::string> SplitIntoWords(const std::string& text);
-
-template <typename StringContainer>
-std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    std::set<std::string> non_empty_strings;
-    for (const std::string& str : strings) {
-        if (!str.empty() && IsValidWord(str)) {
-            non_empty_strings.insert(str);
-        }
-    }
-    return non_empty_strings;
-}
