ddc1fc70a653ed00ddc64ea19cad0a60bf98ec2d
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:19:23 +0300

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,7 +1,5 @@
 #include "request_queue.h"
 
-namespace std {
-    uint RequestQueue::GetNoResultRequests() const {
-        return no_result_requests_;
-    }
-} //namespace std
+uint RequestQueue::GetNoResultRequests() const {
+    return no_result_requests_;
+}
