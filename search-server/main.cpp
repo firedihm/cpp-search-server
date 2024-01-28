41a5b97dccb241ba1c84065da21fd0cd2db57ca9
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:03:49 +0300

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,69 +1,9 @@
-#include "search_server.h"
-
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stdexcept>
-#include <string>
-#include <utility>
-
-class RequestQueue {
-public:
-    explicit RequestQueue(const SearchServer& search_server)  : search_server_(search_server), no_result_requests_(0) {}
-    
-    //вызов без явно переданного статуса или предиката = вызов с предикатом по умолчанию
-    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus sought_status = DocumentStatus::ACTUAL) {
-        return AddFindRequest(raw_query, [=](int document_id, DocumentStatus status, int rating) {
-                                                  return status == sought_status;
-                                              });
-    }
-    
-    template <typename DocumentPredicate>
-    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate predicate) {
-        vector<Document> result = search_server_.FindTopDocuments(raw_query, predicate);
-        
-        //избавляемся от записей, старше суток
-        if (requests_.size() == MIN_IN_DAY) {
-            if (requests_.front().empty()) {
-                --no_result_requests_;
-            }
-            requests_.pop();
-        }
-        
-        requests_.emplace(raw_query, result.size());
-        
-        if (result.empty()) {
-            ++no_result_requests_;
-        }
-        
-        return result;
-    }
-    
-    uint GetNoResultRequests() const {
-        return no_result_requests_;
-    }
-    
-private:
-    const static size_t MIN_IN_DAY = 1440;
-    
-    struct QueryResult {
-        string query;
-        bool is_empty;
-        
-        QueryResult(const string& query, int documents_found) : query(query), is_empty(!documents_found) {}
-        
-        bool empty() { return is_empty; }
-    };
-    
-    const SearchServer& search_server_;
-    queue<QueryResult> requests_;
-    uint no_result_requests_;
-};
-
-// ==================== для примера =========================
+#include "request_queue.h"
+//#include "search_server.h"
 
 int main() {
+    using namespace std;
+    
     SearchServer search_server("and in at"s);
     RequestQueue request_queue(search_server);
     search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
