df9be9089bd81b18d5e8673bb8e20836527a5357
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 15:23:12 +0300

    Update paginator.h

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -1,45 +1,43 @@
 #pragma once
 
-namespace std {
-    template <typename Iterator>
-    class Paginator {
-    public:
-        Paginator(Iterator begin, Iterator end, size_t page_size) {
-            size_t docs_left = distance(begin, end);
-            while (docs_left) {
-                size_t docs_per_current_page = min(page_size, docs_left);
-                pages_.emplace_back(begin, begin + docs_per_current_page);
-                
-                begin += docs_per_current_page;
-                docs_left -= docs_per_current_page;
-            }
+template <typename Iterator>
+class Paginator {
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        size_t docs_left = distance(begin, end);
+        while (docs_left) {
+            size_t docs_per_current_page = min(page_size, docs_left);
+            pages_.emplace_back(begin, begin + docs_per_current_page);
+            
+            begin += docs_per_current_page;
+            docs_left -= docs_per_current_page;
         }
+    }
+    
+    auto   begin() const { return pages_.begin(); }
+    auto   end()   const { return pages_.end(); }
+    bool   empty() const { return pages_.empty(); }
+    size_t size()  const { return pages_.size(); }
+    
+private:
+    struct IteratorRange {
+        Iterator begin;
+        Iterator end;
         
-        auto   begin() const { return pages_.begin(); }
-        auto   end()   const { return pages_.end(); }
-        bool   empty() const { return pages_.empty(); }
-        size_t size()  const { return pages_.size(); }
+        IteratorRange(Iterator begin, Iterator end) : begin(begin), end(end) {}
         
-    private:
-        struct IteratorRange {
-            Iterator begin;
-            Iterator end;
-            
-            IteratorRange(Iterator begin, Iterator end) : begin(begin), end(end) {}
-            
-            friend ostream& operator<<(ostream& os, const IteratorRange& range) {
-                for (auto it = range.begin; it != range.end; ++it) {
-                    os << *it; //it->Document
-                }
-                return os;
+        friend std::ostream& operator<<(std::ostream& os, const IteratorRange& range) {
+            for (auto it = range.begin; it != range.end; ++it) {
+                os << *it; //it->Document
             }
-        };
-        
-        vector<IteratorRange> pages_;
+            return os;
+        }
     };
     
-    template <typename Container>
-    auto Paginate(const Container& c, size_t page_size) {
-        return Paginator(c.begin(), c.end(), page_size);
-    }
-} //namespace std
+    vector<IteratorRange> pages_;
+};
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(c.begin(), c.end(), page_size);
+}
