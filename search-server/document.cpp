ff4dfe53443c2aa6ce3fe98174249f985df1540f
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-28 17:22:23 +0300

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -1,11 +1,55 @@
 #include "document.h"
 
-std::ostream& Document::operator<<(std::ostream& os) const {
-    os << "{ "s
-        << "document_id = "s << this->id << ", "s
-        << "relevance = "s << this->relevance << ", "s
-        << "rating = "s << this->rating
-        << " }"s;
+std::ostream& operator<<(std::ostream& os, const DocumentStatus& status) {
+    using namespace std::string_literals;
     
+    switch (status) {
+        case DocumentStatus::ACTUAL:
+            os << "ACTUAL"s;
+            break;
+        case DocumentStatus::IRRELEVANT:
+            os << "IRRELEVANT";
+            break;
+        case DocumentStatus::BANNED:
+            os << "BANNED";
+            break;
+        case DocumentStatus::REMOVED:
+            os << "REMOVED";
+            break;
+    }
     return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Document& document) {
+    using namespace std::string_literals;
+    
+    os << "{ "s
+       << "document_id = "s << document.id << ", "s
+       << "relevance = "s << document.relevance << ", "s
+       << "rating = "s << document.rating
+       << " }"s;
+    return os;
+}
+
+void PrintDocument(const Document& document) {
+    using namespace std;
+    
+    cout << "{ "s
+         << "document_id = "s << document.id << ", "s
+         << "relevance = "s << document.relevance << ", "s
+         << "rating = "s << document.rating
+         << " }"s << endl;
+}
+
+void PrintMatchDocumentResult(int id, const std::vector<std::string>& words, DocumentStatus status) {
+    using namespace std;
+    
+    cout << "{ "s
+         << "document_id = "s << id << ", "s
+         << "status = "s << status << ", "s
+         << "words ="s;
+         for (const auto& word : words) {
+             cout << ' ' << word;
+         }
+    cout << "}"s << endl;
 }
