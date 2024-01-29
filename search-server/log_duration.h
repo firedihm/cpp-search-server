f47c87562fd65ff334fa0a655a8dd81daa8945d4
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-29 14:31:59 +0300

    Update log_duration.h

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -4,10 +4,13 @@
 #include <iostream>
 #include <string>
 
+#define PROFILE_CONCAT_INTERNAL(X, Y) X ## Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+
 class LogDuration {
 public:
-    // заменим имя типа std::chrono::steady_clock
-    // с помощью using для удобства
     using Clock = std::chrono::steady_clock;
     
     LogDuration(const std::string& msg) : msg_(msg) {}
