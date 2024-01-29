1be1759b1702d90195f074fe39b7e6f8574bd6ef
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-29 20:40:39 +0300

    Update search_server.cpp

--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -1,6 +1,9 @@
 #include "search_server.h"
 
+#include <functional>
 #include <numeric>
+
+using namespace std::string_literals;
 
 const std::set<std::string>& SearchServer::GetStopWords() const {
     return stop_words_;
@@ -35,19 +38,15 @@ double SearchServer::ComputeWordInverseDocumentFreq(const std::string& word) con
 }
 
 void SearchServer::AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings) {
-    using namespace std::string_literals;
-    
     if (document_id < 0 || documents_.count(document_id) != 0) {
         throw std::invalid_argument("document id "s + std::to_string(document_id) + " is invalid or already exists"s);
     }
     
     std::vector<std::string> words = ParseDocument(document);
-    /*
-    не проходит тесты, хотя решение хорошее...
-    if (words.empty()) {
-        throw std::invalid_argument("document id "s + std::to_string(document_id) + " has no valid plus words"s);
-    }
-    */
+    //if (words.empty()) {
+    //    throw std::invalid_argument("document id "s + std::to_string(document_id) + " has no valid plus words"s);
+    //}
+    
     const double inv_word_count = 1.0 / words.size();
     for (const std::string& word : words) {
         word_to_document_freqs_[word][document_id] += inv_word_count;
@@ -58,8 +57,6 @@ void SearchServer::AddDocument(int document_id, const std::string& document, Doc
 }
 
 std::vector<std::string> SearchServer::ParseDocument(const std::string& text) const {
-    using namespace std::string_literals;
-    
     std::vector<std::string> words;
     for (const std::string& word : SplitIntoWords(text)) {
         if (!IsValidWord(word)) {
@@ -70,6 +67,30 @@ std::vector<std::string> SearchServer::ParseDocument(const std::string& text) co
         }
     }
     return words;
+}
+
+template <typename DocumentPredicate>
+std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query, DocumentPredicate predicate) const {
+    std::vector<Document> matched_documents = FindAllDocuments(ParseQuery(raw_query), predicate);
+    
+    std::sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
+         if (std::abs(lhs.relevance - rhs.relevance) < EPSILON) {
+             return lhs.rating > rhs.rating;
+         }
+         return lhs.relevance > rhs.relevance;
+    });
+    
+    if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+        matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+    }
+    return matched_documents;
+}
+template std::vector<Document> SearchServer::FindTopDocuments<const std::string&, std::function<bool(int, DocumentStatus, int)>>(const std::string& raw_query, std::function<bool(int, DocumentStatus, int)> predicate);
+
+std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_query, DocumentStatus sought_status) const {
+    return FindTopDocuments(raw_query, [=](int document_id, DocumentStatus status, int rating) {
+                                           return status == sought_status;
+                                       });
 }
 
 std::tuple<std::vector<std::string>, DocumentStatus> SearchServer::MatchDocument(const std::string& raw_query, int document_id) const {
