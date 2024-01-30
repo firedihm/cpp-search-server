b42b62ce775ed81822a5d3a004187b8fe1f3b8b7
firedihm <42544757+firedihm@users.noreply.github.com>
2024-01-30 12:06:23 +0300

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -3,3 +3,9 @@
 uint RequestQueue::GetNoResultRequests() const {
     return no_result_requests_;
 }
+
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus sought_status) {
+    return AddFindRequest(raw_query, [=](int document_id, DocumentStatus status, int rating) {
+                                              return status == sought_status;
+                                     });
+}
