22b06e8e4c7670195f75548aee32336f67d2ce23
firedihm <42544757+firedihm@users.noreply.github.com>
2024-02-21 16:18:08 +0300

    Update string_processing.cpp

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -19,3 +19,17 @@ std::vector<std::string> SplitIntoWords(const std::string& text) {
     
     return words;
 }
+
+vector<string_view> SplitIntoWordsView(string_view str) {
+    vector<string_view> result;
+    for(size_t caret; (caret = str.find_first_not_of(' ')) != str.npos; ) {
+        str.remove_prefix(min(str.size(), caret));
+        
+        caret = str.find(' ');
+        result.push_back(str.substr(0, min(str.size(), caret)));
+        
+        str.remove_prefix(min(str.size(), caret));
+    }
+    
+    return result;
+}
